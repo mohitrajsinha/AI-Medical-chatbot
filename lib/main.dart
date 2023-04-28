b75e52cb6083bc4d7896c9330fc8847241181060
mohitrajsinha <mohitrajsinha321@gmail.com>
2023-04-28 12:13:47 +0530

    project added

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'chat_screen.dart';
+
+// Future main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await dotenv.load(fileName: ".env");
+//   runApp(const MyApp());
+// }
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: "assets/.env");
+    // dotenv.get('apikey', fallback: "API_KEY NOT FOUND");
+
+  // final apiKey = dotenv.env['apikey'];
+  // print(apiKey);
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AI Medical Assistant',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        useMaterial3: true,
+      ),
+      home: const ChatScreen(),
+    );
+  }
+}
